b8b9447a451e6bebb662c8dfcf982dd014a31746
ABUL KALAM AZAD <circleup.kalam@gmail.com>
2022-02-12 22:33:22 +0600

    first commite

---
+++ bin/lecture_01.dart
@@ -0,0 +1,28 @@
+import 'dart:ffi';
+
+void main(List<String> arguments) {
+  // int age = 1;
+  // double workingTime= 2.4;
+  // String name = 'ABUL KALAM AZAD';
+  // bool akazad = true;
+  // List akaazad = [];
+  // Map akazsad ={};
+  // String name = 'ABUL KALAM AZAD';
+  // int age = 20;
+  // double cgpa = 3.5;
+  //
+  // print('Name: $name Age : $age Cgpa: $cgpa');
+  // print('Name: $name \nAge : $age \nCgpa: $cgpa');
+
+int value1=100;
+int value2=80;
+int m=value1-value2;
+
+int m2=value1+value2;
+int m3=value1*value2;
+double m4=value1/value2;
+
+print('$value1-$value2=$m');
+}
+
+
